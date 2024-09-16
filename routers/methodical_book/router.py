3fd2e5a511534531f31a21e2113f047d80c721ea
Сергеев Данила Андреевич <da_sergeev@controlsystems.ru>
2024-09-17 00:09:10 +0300

    Поправил эндпоинты и схемы под новую таблицу разделов методички

--- routers/methodical_book/router.py
+++ routers/methodical_book/router.py
@@ -12,58 +12,40 @@ from misc import file_work, verify_data
 
 methodical_book_router = APIRouter(prefix='/methodical_book', tags=['methodical_book'])
 
+@methodical_book_router.get('/chapters/mains', tags=['methodical_book'])
+async def get_main_chapters() -> list[mb_schemes.MethodicalChaptersResponse]:
 
-@methodical_book_router.get('/sections/', tags=['methodical_book'])
-async def get_sections_methodical_book() -> list[mb_schemes.MethodicalSectionsResponse]:
-
-    return await BaseCruds.get_all_data(model=models.SectionsMethodologicalBook,
-                                        schema=mb_schemes.MethodicalSectionsResponse)
-
-
-@methodical_book_router.post('/sections/', tags=['methodical_book'])
-async def create_section_methodical_book(data: mb_schemes.MethodicalSection):
-
-    await verify_data.verify_data(
-        data=data,
-        schema=mb_schemes.MethodicalSection,
-        model=models.SectionsMethodologicalBook,
-        error_msg='Такая секция уже существует',
-        title=data.title
+    return await BaseCruds.get_data_by_filter(
+        model=models.MethodicalBookChapters,
+        schema=mb_schemes.MethodicalChaptersResponse,
+        parent_id=None
     )
 
-    if await BaseCruds.insert_data(model=models.SectionsMethodologicalBook,
-                                   title=data.title):
+@methodical_book_router.get('/chapters/get_childs/{id_chapter}', tags=['methodical_book'])
+async def get_child_chapters(id_chapter: int) -> list[mb_schemes.MethodicalChaptersResponse]:
 
-        return HTTPException(status_code=201,
-                             detail=f'Секция "{data.title}" создана')
-
-    return HTTPException(status_code=500,
-                         detail='Возникла ошибка, попробуйте позже')
-
-
-@methodical_book_router.get('/sections/{section_id}', tags=['methodical_book'])
-async def get_chapters_by_section(section_id: int) -> list[mb_schemes.MethodicalChaptersResponse]:
-
-    return await BaseCruds.get_data_by_filter(model=models.ChaptersMethodologicalBook,
-                                              section_id=section_id,
-                                              schema=mb_schemes.MethodicalChaptersResponse)
+    return await BaseCruds.get_data_by_filter(
+        model=models.MethodicalBookChapters,
+        schema=mb_schemes.MethodicalChaptersResponse,
+        parent_id=id_chapter
+    )
 
 
 @methodical_book_router.post('/chapters/', tags=['methodical_book'])
-async def create_chapter_methodical_book(title_chapter: Annotated[str, Form()],
-                                         section_id: Annotated[int, Form()],
+async def create_chapter_methodical_book(title: Annotated[str, Form()],
+                                         parent_id: Annotated[int, Form()],
                                          file: Annotated[UploadFile, File()]):
 
     if file_work.save_file(path='database/files_data/methodical_data/',
                            file=file):
 
         data = {
-            'title': title_chapter,
-            'section_id': section_id,
+            'title': title,
+            'parent_id': parent_id,
             'file_path': f'database/files_data/methodical_data/{file.filename}'
         }
 
-        if await BaseCruds.insert_data(model=models.ChaptersMethodologicalBook, **data):
+        if await BaseCruds.insert_data(model=models.MethodicalBookChapters, **data):
             return 'Сохранил'
 
         return 'Кабзда'
@@ -75,7 +57,7 @@ async def create_chapter_methodical_book(title_chapter: Annotated[str, Form()],
 @methodical_book_router.get('/chapters/file/{chapter_id}', tags=['methodical_book'])
 async def get_chapter_file(chapter_id: int) -> FileResponse:
 
-    chapter = await BaseCruds.get_data_by_id(model=models.ChaptersMethodologicalBook,
+    chapter = await BaseCruds.get_data_by_id(model=models.MethodicalBookChapters,
                                              model_id=chapter_id,
                                              schema=mb_schemes.MethodicalChaptersResponse)
 
