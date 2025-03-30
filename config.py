752878d7dc5e5fb00ed671bfb0252f8a671a42c8
Сергеев Данила Андреевич <da_sergeev@controlsystems.ru>
2025-03-30 23:00:33 +0800

    Переделка под Vault

--- config.py
+++ config.py
@@ -1,20 +1,58 @@
+import hvac
 import os
+
 from dotenv import load_dotenv
+from hvac.exceptions import InvalidPath
+from common_lib.logger import logger
 
 load_dotenv()
 
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_NAME = os.environ.get('DB_NAME')
+client = hvac.Client(
+    url='http://127.0.0.1:8200',
+    token=os.environ.get('VAULT_TOKEN'),
+)
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY_REFRESH = os.environ.get('SECRET_KEY_REFRESH')
+try:
+    postgres_data = client.secrets.kv.v2.read_secret_version(
+        path="Postgres", # Имя секрета
+        mount_point='pipif' # Имя engine
+    )
+    DB_USER = postgres_data['data']['data']['DB_USER']
+    DB_PASS = postgres_data['data']['data']['DB_PASS']
+    DB_HOST = postgres_data['data']['data']['DB_HOST']
+    DB_PORT = int(postgres_data['data']['data']['DB_PORT'])
+    DB_NAME = postgres_data['data']['data']['DB_NAME']
+except InvalidPath:
+    logger.critical('Не найдены параметры подключения к БД Postgres')
 
-GRAFANA_HOST = os.environ.get('GRAFANA_HOST')
-GRAFANA_PORT = os.environ.get('GRAFANA_PORT')
-GRAFANA_TOKEN = os.environ.get('GRAFANA_TOKEN')
+try:
+    jwt_data = client.secrets.kv.v2.read_secret_version(
+        path="Pass_secrets",
+        mount_point='pipif'
+    )
+    SECRET_KEY = jwt_data['data']['data']['SECRET_KEY']
+    SECRET_KEY_REFRESH = jwt_data['data']['data']['SECRET_KEY_REFRESH']
+except InvalidPath:
+    logger.critical('Не найдены параметры JWT')
 
-S3_ACCESS_KEY = os.environ.get('S3ACCESSKEY')
-S3_SECRET_KEY = os.environ.get('S3SECRETKEY')
+try:
+    grafana_data = client.secrets.kv.v2.read_secret_version(
+        path="Grafana",
+        mount_point='pipif'
+    )
+    GRAFANA_HOST = grafana_data['data']['data']['GRAFANA_HOST']
+    GRAFANA_PORT = int(grafana_data['data']['data']['GRAFANA_PORT'])
+    GRAFANA_TOKEN = grafana_data['data']['data']['GRAFANA_TOKEN']
+except InvalidPath:
+    logger.critical('Не найдены параметры для Grafana')
+
+try:
+    s3_data = client.secrets.kv.read_secret_version(
+        path="S3",
+        mount_point='pipif'
+    )
+    S3_ACCESS_KEY = s3_data['data']['data']['S3ACCESSKEY']
+    S3_SECRET_KEY = s3_data['data']['data']['S3SECRETKEY']
+    S3_SSL_CERT = s3_data['data']['data']['S3SSLCERT']
+except InvalidPath:
+    logger.critical('Не найдены параметры для S3')
